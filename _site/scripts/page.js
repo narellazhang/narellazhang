57c9d6435352dd696472a3842c8ae609de36cea9
narella <zhangnanmei@126.com>
2014-09-23 16:43:32 +0800

    update:delete some useless code

--- _site/scripts/page.js
+++ _site/scripts/page.js
@@ -1,25 +1,65 @@
 $(function(){
-	$(".nav li").click(function(){
-		console.log(this);
-		$('this').toggleClass("active");
-		var liclass = $(this).attr("class").split(" ");
-		console.log(liclass);
-		// 
-		for(var i=0;i<liclass.length;i++){
-			if(liclass[i]!='icon'&&liclass[i]!='active'){
-			    var bgPosition = $(liclass[i]).css('background-position');
-			    if(typeof(bgPosition) == 'undefined') {
-			     	bgPosition = $(liclass[i]).css('background-positionX') + ' ' + $(liclass[i]).css('background-positionY');
-			    }
-				bgPosition=bgPosition.split(" ") //转成字符串数组
-				var x = bgPosition[0];
-				var y = bgPosition[1];
-				 
-				var position = parseInt(x-50)+"px "+ y+"px";//右移50px
-				    alert(position);
-				    $(liclass[i]).css({backgroundPosition:position}); 
-			}
-		}
-		
-	});	
+	
+
+	 (new CenterImgPlay()).Start();
+
 })
+function CenterImgPlay() {
+            this.list = $(".imgbox").children(":first").children();
+            this.indexs = [];
+            this.length = this.list.length;
+            //图片显示时间
+            this.timer = 3000;
+            this.showTitle = $(".imgbox .title");
+
+            var index = 0, self = this, pre = 0, handid, isPlay = false, isPagerClick = false;
+
+            this.Start = function () {
+                this.Init();
+                //计时器，用于定时轮播图片
+                handid = setInterval(self.Play, this.timer);
+            };
+            //初始化
+            this.Init = function () {
+                var o = $(".pager ul li"), _i;
+
+                for (var i = o.length - 1, n = 0; i >= 0; i--, n++) {
+                    this.indexs[n] = o.eq(i).click(self.PagerClick);
+                }
+            };
+            this.Play = function () {
+                isPlay = true;
+                index++;
+                if (index == self.length) {
+                    index = 0;
+                }
+                //先淡出，在回调函数中执行下一张淡入
+                self.list.eq(pre).fadeOut(300, "linear", function () {
+                    var info = self.list.eq(index).fadeIn(500, "linear", function () {
+                        isPlay = false;
+                        if (isPagerClick) { handid = setInterval(self.Play, self.timer); isPagerClick = false; }
+                    }).attr("title");
+                    //显示标题
+                    self.showTitle.text(info);
+                    //图片序号背景更换
+                    self.indexs[index].css("background-color", "#fcd0ab");
+                    self.indexs[pre].css("background-color", "#50382b");
+
+                    pre = index;
+                });
+            };
+            //图片序号点击
+            this.PagerClick = function () {
+                if (isPlay) { return; }
+                isPagerClick = true;
+
+                clearInterval(handid);
+
+                var oPager = $(this), i = parseInt(oPager.text()) - 1;
+
+                if (i != pre) {
+                    index = i - 1;
+                    self.Play();
+                }
+            };
+        };
